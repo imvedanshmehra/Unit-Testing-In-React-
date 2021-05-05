71783d7e6dfd59ef8d9ef4d574a82f19830e50ca
Vedansh Mehra <vedanshofficial@gmail.com>
2021-05-05 17:58:40 +0530

    created test for the btn click event

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,12 @@
+import React, { useState } from 'react';
+
 const Button = () => {
-  return <button title="testBtn">Search</button>;
+  const [value, setValue] = useState('Click Me');
+  return (
+    <button onClick={() => setValue('You Clicked')} title="testBtn">
+      {value}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
 it('checkButtonRender', () => {
@@ -6,4 +6,14 @@ it('checkButtonRender', () => {
   const btn = queryByTitle('testBtn');
 
   expect(btn).toBeTruthy();
+});
+
+describe('btnClicked', () => {
+  it('onClick', () => {
+    const { queryByTitle } = render(<Button />);
+    const btn = queryByTitle('testBtn');
+    expect(btn.innerHTML).toBe('Click Me');
+    fireEvent.click(btn);
+    expect(btn.innerHTML).toBe('You Clicked');
+  });
 });
