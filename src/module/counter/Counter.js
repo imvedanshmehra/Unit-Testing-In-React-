02e3148dae5b824f59addcd9c6e9e85e6f119c7d
Vedansh Mehra <vedanshofficial@gmail.com>
2021-05-12 23:21:09 +0530

    wrote test for increment counter btn

--- src/module/counter/Counter.js
+++ src/module/counter/Counter.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const incrementCounter = () => {
-        setCount(count + 1)
-    }
-
-    const decrementCounter = () => {
-        setCount(count -1)
-    }
-
-    return (
-        <div data-test="counter">
-           <h1>The count is <span data-test='counter-val'>{count}</span></h1> 
-           <span>
-               <button onClick={incrementCounter}>+</button>
-               <button onClick={decrementCounter}>-</button>
-           </span>
-        </div>
-    )
+  return (
+    <div data-test="counter">
+      <h1>
+        The count is <span data-test="counter-val">{count}</span>
+      </h1>
+      <span>
+        <button data-test="increment-btn" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+        <button>-</button>
+      </span>
+    </div>
+  );
 }
--- src/module/counter/Counter.test.js
+++ src/module/counter/Counter.test.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import Counter from "./Counter";
-import Enzyme, {shallow} from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17"
+import Counter from './Counter';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-Enzyme.configure({adapter : new EnzymeAdapter() });
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('renders non-empty Counter without crashing', () => {
-const wrapper = shallow(<Counter />);
-const counterDisplay = wrapper.find("[data-test='counter']");
-expect(counterDisplay.length).toBe(1)
+  const wrapper = shallow(<Counter />);
+  const counterDisplay = wrapper.find("[data-test='counter']");
+  expect(counterDisplay.length).toBe(1);
 });
 
+test('counter value starts at 0', () => {
+  const wrapper = shallow(<Counter />);
+  const counterVal = wrapper.find("[data-test='counter-val']").text();
+  expect(counterVal).toBe('0');
+});
 
-test('counter value starts at 0', () =>{
-const wrapper = shallow(<Counter />);
-const counterVal = wrapper.find("[data-test='counter-val']").text();
-expect(counterVal).toBe("0")
-}); 
+test('increment counter value on click of increment btn', () => {
+  // find increment btn
+  const wrapper = shallow(<Counter />);
+  const incrementBtn = wrapper.find("[data-test='increment-btn']");
+
+  // click increment btn
+  incrementBtn.simulate('click');
+
+  // increment and test the value of counter value
+  const counterVal = wrapper.find("[data-test='counter-val']").text();
+  expect(counterVal).toBe('1');
+});
